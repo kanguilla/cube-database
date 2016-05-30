b7412ec0792f2933f982e957c69f64f783ed49bd
khalilvanalphen <khalilvanalphen@kva-macbook.youilabs.local>
2016-05-30 16:36:39 -0400

    lazy loading

--- Cube Database/src/main/java/CardDialog.java
+++ Cube Database/src/main/java/CardDialog.java
@@ -1,11 +1,14 @@
 package main.java;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -37,29 +40,46 @@ public class CardDialog extends Stage{
 			    String number = rs.getString("number");
 			    String setName = rs.getString("setName");
 				System.out.println("   " + card.name + "...");
-				String imageUrl = "http://magiccards.info/scans/en/" +mciCode+ "/" + number + ".jpg";
-				URLConnection hc = new URL(imageUrl).openConnection();
-				hc.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
-				hc.connect();
-				Image img = new Image(hc.getInputStream());
 				
 				Tab tab = new Tab(setName);
-				tab.setContent(new ImageView(img));
 				tab.setClosable(false);
-				
 				tabPane.getTabs().add(tab);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		tabPane.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+		        Tab t = tabPane.getTabs().get(newValue.intValue());
+		    	if (t.getContent() == null){
+					t.setContent(new ImageView(loadImage(mciCode, number)));
+		        }
+		    }
+		}); 
+		
 		layout.getChildren().add(tabPane);
 		
 		Scene dialogScene = new Scene(layout, 300, 480);
 		setScene(dialogScene);
 
 		
+		
+	}
+	
+	public Image loadImage(String mciCode, String number){
+		String imageUrl = "http://magiccards.info/scans/en/" +mciCode+ "/" + number + ".jpg";
+		URLConnection hc;
+		try {
+			hc = new URL(imageUrl).openConnection();
+			hc.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+			hc.connect();
+			return new Image(hc.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 		
 	}
 
